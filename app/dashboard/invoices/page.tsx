f763daaaa799b1ca524a64d7add6ec0dc7d64008
zZz-ren <dhirenrathod0@gmail.com>
2024-09-27 12:10:08 +0530

    Init

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,7 @@
+import { Router } from "next/router";
+
+export default function Invoices() {
+  // const router = new Router;
+
+  return <p>Invoices</p>;
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default function dashboard() {
+  return (
+    <>
+      <p>Dashboard</p>
+    </>
+  );
+}
